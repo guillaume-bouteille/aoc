6afd23b09f08fb24a69b246137ac5bdceefd1252
Guillaume Bouteille <guillaume@bouteille.me.uk>
2023-12-12 22:55:56 +0100

    Mess around with slices and lifetimes

--- 2023/day12/main.rs
+++ 2023/day12/main.rs
@@ -6,7 +6,6 @@ use std::collections::HashMap;
 
 type Line = (Vec<char>, Vec<i64>);
 type Inputs = Vec<Line>;
-type Cache = HashMap<(Vec<char>,Vec<i64>), i64>;
 
 fn parse_inputs() -> (Inputs,Inputs) {
     let mut v1 = Vec::new();
@@ -19,21 +18,22 @@ fn parse_inputs() -> (Inputs,Inputs) {
             let mut m5 = Vec::new();
             let mut d5 = Vec::new();
             for _ in 0..4 {
-                m5.extend(m.clone());
+                m5.extend(&m[..]);
                 m5.push('?');
-                d5.extend(d.clone());
+                d5.extend(&d[..]);
             }
-            m5.extend(m.clone());
-            d5.extend(d.clone());
+            m5.extend(&m[..]);
+            d5.extend(&d[..]);
             v1.push((m, d));
             v2.push((m5, d5));
-            
         }
     }
     (v1,v2)
 }
 
-fn visit_possibilities_v2(map: &Vec<char>, defective_groups: &Vec<i64>, start_idx: usize, cache: &mut Cache) -> i64 {
+fn visit_possibilities_v2<'a, 'b>(
+    map: &'a Vec<char>, defective_groups: &'a [i64], start_idx: usize,
+    cache: &'b mut HashMap<(&'a [char], &'a [i64]), i64>) -> i64 {
 
     if defective_groups.len() == 0 {
         for idx in start_idx..map.len() {
@@ -44,9 +44,7 @@ fn visit_possibilities_v2(map: &Vec<char>, defective_groups: &Vec<i64>, start_id
         return 1;
     }
 
-    let sl : Vec<char> = map.get(start_idx..).expect("tutu").into_iter()
-        .map(|c| *c).collect::<Vec<char>>();
-    if let Some(sol) = cache.get(&(sl.clone(),defective_groups.clone())) {
+    if let Some(sol) = cache.get(&(&map[start_idx..],defective_groups)) {
         return *sol;
     }
 
@@ -62,8 +60,7 @@ fn visit_possibilities_v2(map: &Vec<char>, defective_groups: &Vec<i64>, start_id
     }
 
     let next_group_size = defective_groups[0] as usize;
-    let mut new_defective_groups : Vec<i64> = defective_groups.clone();
-    new_defective_groups.remove(0);
+    let new_defective_groups = &defective_groups[1..];
 
     let space_occupied: i64 = defective_groups.iter().sum::<i64>()
         + (defective_groups.len() as i64) - 1;
@@ -94,15 +91,15 @@ fn visit_possibilities_v2(map: &Vec<char>, defective_groups: &Vec<i64>, start_id
         }
     }
 
-    cache.insert((sl,defective_groups.clone()), sum);
+    cache.insert((&map[start_idx..],defective_groups), sum);
 
     sum
 }
 
-fn compute_nb_possibilities(map: &Vec<char>, defective_groups: &Vec<i64>) -> i64 {
+fn compute_nb_possibilities<'a>(map: &'a Vec<char>, defective_groups: &'a Vec<i64>) -> i64 {
 
-    let mut cache : Cache = HashMap::new();
-    visit_possibilities_v2(map, defective_groups, 0, &mut cache)
+    let mut cache = HashMap::new();
+    visit_possibilities_v2(map, &defective_groups[..], 0, &mut cache)
 }
 
 fn main() {
@@ -111,10 +108,10 @@ fn main() {
 
     let mut p1 : i64 = 0;
     let mut p2 : i64 = 0;
-    for l in inputs_p1 {
+    for l in inputs_p1.iter() {
         p1 += compute_nb_possibilities(&l.0, &l.1);
     }
-    for l in inputs_p2 {
+    for l in inputs_p2.iter() {
         p2 += compute_nb_possibilities(&l.0, &l.1);
     }
     println!("P1={}", p1);
